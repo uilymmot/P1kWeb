89253c3b0946d289b51b757ff31724c0eac67f24
Tommy Liu <u6409902@anu.edu.au>
2017-12-21 10:26:26 +1300

    added first page

---
+++ resources/views/pages/page1.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet"
+          href="{{asset('css/startPage.css')}}">
+    <meta charset="UTF-8">
+    <title> Page 1</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="h1"><?php
+            echo 'page opened on: ' . date('m/d/Y h:i:s a', time());
+            ?></div>
+    </div>
+
+</body>
+</html>
