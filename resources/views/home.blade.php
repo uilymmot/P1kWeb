9b02dacf5116711a5ae76bd15c32c3b89119df93
Tommy Liu <u6409902@anu.edu.au>
2017-12-21 13:33:00 +1300

    Added layouts and sections

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex-center position-ref" style="height: 100vh">
+
+        <div class="content">
+            <div class="title m-b-md">
+                <a href="/">Uilymmot</a>
+            </div>
+
+            <div class="wrap1">
+                <div class="links">
+                    <a href="about" >About</a>
+                </div>
+
+                <div class="links">
+                    <a href="projects">Projects</a>
+                </div>
+
+                <div class="links">
+                    <a href="https://github.com/uilymmot">GitHub</a>
+                </div>
+
+                <div class="links">
+                    <a href="page1">Random Rest of website</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link type="text/css" rel="stylesheet"
-          href="css/startPage.css">
-    <title>Uilymmot</title>
+@extends('layouts.app')
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-</head>
-<body  style="background: lightgray">
+@section('content')
 <div class="flex-center position-ref full-height" style="vertical-align: top; display: block; ">
 
     <div class="content">
@@ -141,5 +129,4 @@
             </p>
     </div>
 </div>
-</body>
-</html>
+@endsection
