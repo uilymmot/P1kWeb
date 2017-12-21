35fe7dd3e50d2c071ca71143aa06e6075ef68025
Tommy Liu <u6409902@anu.edu.au>
2017-12-21 14:42:54 +1300

    added projects page (everything is wip)

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -1,10 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+@extends('layouts.app')
 
-</body>
-</html>
+@section('content')
+    <div class="flex-center position-ref full-height" style="vertical-align: top; display: block; ">
+
+        <div class="content">
+            <div class="title m-b-md">
+                <a href="/">Uilymmot</a>
+            </div>
+
+            <div class="wrap1">
+                <div class="links">
+                    <a href="about" >About</a>
+                </div>
+
+                <div class="links">
+                    <a href="projects">Projects</a>
+                </div>
+
+                <div class="links">
+                    <a href="https://github.com/uilymmot">GitHub</a>
+                </div>
+
+                <div class="links">
+                    <a href="page1">Random Rest of website</a>
+                </div>
+            </div>
+
+            <div class="content">
+                <div class="ctr2">
+                    <div class="itm1" style="float: left">Project1</div>
+                    <div class="itm1" style="float: right;">Project1</div>
+                </div>
+
+                <div class="ctr2">
+                    <div class="itm1" style="float: left">Project1</div>
+                    <div class="itm1" style="float: right;">Project1</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
