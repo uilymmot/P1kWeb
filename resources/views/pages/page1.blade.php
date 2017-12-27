d169cbb2d2112b531f75e5f899dd43f06a618895
Tommy Liu <u6409902@anu.edu.au>
2017-12-27 14:44:54 +1300

    added more features

--- resources/views/pages/page1.blade.php
+++ resources/views/pages/page1.blade.php
@@ -8,11 +8,23 @@
           href="{{asset('css/startPage.css')}}">
     <meta charset="UTF-8">
     <script type="text/javascript">
-        function flip() {
+        function flipCoin() {
             var rand = Math.random();
 
             if (rand > 0.5) alert("Heads");
             else alert('Tails');
+        }
+
+        function home() {
+            location.href = '/';
+        }
+
+        function getRandomVal() {
+            var minVal = (document.getElementsByName('textbox1')[0].value);
+            var maxVal = (document.getElementsByName('textbox2')[0].value);
+            var ranVal = Math.random();
+            var ranOut = Math.round(ranVal * (maxVal - minVal));
+            alert(ranOut+1);
         }
     </script>
 
@@ -20,12 +32,21 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="flex-center position-ref full-height">
-    <div class="h1"><?php
-        echo 'page opened on: ' . date('m/d/Y h:i:s a', time());
-        ?></div>
+<div class="flex-center position-ref">
+    <div class="content">
+        <button type="button" onclick="home()">Home</button>
 
-    <button type="button" onclick="flip()" autofocus="autofocus">Flip</button>
+        <div class="h1"><?php
+            echo 'page opened on: ' . date('m/d/Y h:i:s a', time());
+            ?></div>
+
+        <button type="button" onclick="flipCoin()" autofocus="autofocus">Flip</button>
+
+        <br/><br/><br/>
+
+        Get Random Number between: <input name="textbox1" id="textbox1" type="number"> and <input name="textbox2" id="textbox2" type="number">. <button type="button" onclick="getRandomVal()"> get number</button>
+    </div>
+
 
 </div>
 </body>
