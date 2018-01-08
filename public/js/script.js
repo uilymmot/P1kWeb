d64440a017929fa7a997dc5b297cf0b43224641e
Tommy Liu <u6409902@anu.edu.au>
2018-01-09 11:27:12 +1300

    got sass working - gonna rewrite css in scss, also some minor style changes elsewhere

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
 function flipCoin() {
     var rand = Math.random();
-    if (rand > 0.5) alert("Heads");
-    else alert('Tails');
+    if (rand > 0.5) {
+        alert("Heads");
+        drawOnCan("Heads")
+    }
+    else {
+        alert('Tails');
+        drawOnCan('Tails');
+    }
 }
 
 function home() {
@@ -22,7 +28,7 @@ function drawOnCan(num) {
     var ctx = thing.getContext("2d");
     var yPos = Math.round(Math.random() * document.getElementById("valCan").clientHeight);
     var xPos = Math.round(Math.random() * document.getElementById("valCan").clientWidth);
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "#11EE15";
+    ctx.font = "18px Arial";
+    ctx.fillStyle = "#000000";
     ctx.fillText(num, +xPos, +yPos);
 }
