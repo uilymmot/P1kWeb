2d41efaa14655881681eef94831fb6b111b4b13e
Tommy Liu <u6409902@anu.edu.au>
2018-01-12 11:20:07 +1300

    bug fix - full area working

--- public/js/snakes.js
+++ public/js/snakes.js
@@ -1,63 +1,80 @@
 let xVelocity = 1;
 let yVelocity = 0;
-
+let isRunning = false;
 document.onkeydown = keyPush;
 
 function playGame() {
-    let canvas = document.getElementById("snakes-canvas");
-    let ctx = canvas.getContext("2d");
-    let itemWidth = canvas.height / 25;
+    if (!isRunning) {
+        isRunning = true;
+        let canvas = document.getElementById("snakes-canvas");
+        let ctx = canvas.getContext("2d");
+        let itemWidth = canvas.height / 25;
 
-    let snake = [];
-    for (i = (itemWidth*10); i <= (200 + (itemWidth * 5)); i += itemWidth) {
-        snake.push(new SnakeObject(250, i));
-    }
-
-    window.setInterval(pGame, 200);
-
-    function SnakeObject(x, y) {
-        this.xPos = x;
-        this.yPos = y;
-    }
-
-    function pGame() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        ctx.fillStyle = "#000000";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        ctx.fillStyle = "#1FFF1F";
-        for (i = 0; i < snake.length; i++) {
-            ctx.fillRect(snake[i].xPos, snake[i].yPos, itemWidth, itemWidth);
+        let snake = [];
+        for (i = (itemWidth * 10); i <= (itemWidth*10) + (itemWidth * 5); i += itemWidth) {
+            snake.push(new SnakeObject((itemWidth * 10), i));
         }
 
-        let newX = snake[0].xPos + (xVelocity * itemWidth);
-        let newY = snake[0].yPos + (yVelocity * itemWidth);
-        if (newX >= canvas.width) newX -= canvas.width;
-        if (newY >= canvas.height) newY -= canvas.height;
+        window.setInterval(pGame, 200);
 
-        snake.unshift(new SnakeObject(newX, newY));
-        snake.splice(-1, 1);
+        function SnakeObject(x, y) {
+            this.xPos = x;
+            this.yPos = y;
+        }
+
+        function pGame() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = "#1FFF1F";
+            for (i = 0; i < snake.length; i++) {
+                ctx.fillRect(snake[i].xPos, snake[i].yPos, itemWidth, itemWidth);
+            }
+
+            let newX = snake[0].xPos + (xVelocity * itemWidth);
+            let newY = snake[0].yPos + (yVelocity * itemWidth);
+
+            if (newX > canvas.width+1) newX -= canvas.width;
+            if (newY > canvas.height+1) newY -= canvas.height;
+            if (newX < -1) newX += canvas.width;
+            if (newY < -1) newY += canvas.height;
+
+            snake.unshift(new SnakeObject(newX, newY));
+            snake.splice(-1, 1);
+        }
     }
 }
 
 function keyPush(key) {
     switch (key.keyCode) {
         case 37: // Left arrow
-            xVelocity = -1;
-            yVelocity = 0;
+            if (xVelocity !==1) {
+                xVelocity = -1;
+                yVelocity = 0;
+            }
             break;
         case 38: // Up arrow
-            xVelocity = 0;
-            yVelocity = -1;
+            if (yVelocity!==1) {
+                xVelocity = 0;
+                yVelocity = -1;
+            }
             break;
         case 39: // Right arrow
-            xVelocity = 1;
-            yVelocity = 0;
+            if (xVelocity!==-1) {
+                xVelocity = 1;
+                yVelocity = 0;
+            }
             break;
         case 40: // Down arrow
-            xVelocity = 0;
-            yVelocity = 1;
+            if (yVelocity !==-1) {
+                xVelocity = 0;
+                yVelocity = 1;
+            }
+            break;
+        case 116:
+            location.reload();
             break;
         default:
             break;
